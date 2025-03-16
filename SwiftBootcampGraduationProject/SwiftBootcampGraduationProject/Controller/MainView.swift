40e5c867a3a57751f8562bde564be3a9daf7913d
omer cakar <omerrccak@gmail.com>
2025-03-16 15:40:37 +0300

    Project Launched

---
+++ SwiftBootcampGraduationProject/SwiftBootcampGraduationProject/Controller/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBootcampGraduationProject
+//
+//  Created by ÖMER  on 16.03.2025.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
